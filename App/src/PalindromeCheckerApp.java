0afb5043c7cb19f2e646959aa941b6d2343dcb86
ts6708-tech <ts6708@srmist.edu.in>
2026-02-26 15:45:17 +0530

    Tejas added uc-3 palindromecheckerapp

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,26 +1,35 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args){
 
-        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("UC3: Palindrome Check Using String Reverse");
 
-        // Hardcoded string
-        String word = "madam";
+        // Create Scanner object
+        Scanner scanner = new Scanner(System.in);
 
-        // Reverse the string
+        // Take user input
+        System.out.print("Enter a string: ");
+        String original = scanner.nextLine();
+
+        // Variable to store reversed string
         String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        // Reverse using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
-        // Check if palindrome
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome.");
+        // Compare original and reversed string
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome.");
         } else {
-            System.out.println(word + " is NOT a Palindrome.");
+            System.out.println(original + " is NOT a Palindrome.");
         }
 
-        System.out.println("Program exited successfully.");
+        System.out.println("Program ended.");
+
+        // Close scanner
+        scanner.close();
 
     }
 }
